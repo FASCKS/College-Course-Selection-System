a71efd3d4da109588c9de2b497e3a772f5b3b954
pengxingxie <ezc@live.com>
2021-11-25 09:07:00 +0800

    Initial commit

---
+++ src/main/java/com/pxx/collegecourseselectionsystem/CollegeCourseSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pxx.collegecourseselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeCourseSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeCourseSelectionSystemApplication.class, args);
+    }
+
+}
