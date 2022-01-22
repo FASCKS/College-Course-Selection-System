76f6d0fdf5f4e00fb2f601979ef1defcf0ba67b1
pengxingxie <ezc@live.com>
2022-01-22 21:18:58 +0800

    优化ip类

--- src/main/java/com/pxx/collegecourseselectionsystem/common/utils/IPUtils.java
+++ src/main/java/com/pxx/collegecourseselectionsystem/common/utils/IPUtils.java
@@ -10,7 +10,6 @@ package com.pxx.collegecourseselectionsystem.common.utils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,36 +28,79 @@ public class IPUtils {
 	 * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
 	 */
 	public static String getIpAddr(HttpServletRequest request) {
-    	String ip = null;
-        try {
-            ip = request.getHeader("x-forwarded-for");
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
+        String ip = request.getHeader("x-forwarded-for");
+
+        if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+            if (ip.indexOf(",") != -1) {
+                ip = ip.split(",")[0];
             }
-            if (StringUtils.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-            }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            }
-            if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-            }
-        } catch (Exception e) {
-        	logger.error("IPUtils ERROR ", e);
         }
-        
-//        //使用代理，则获取第一个IP地址
-//        if(StringUtils.isEmpty(ip) && ip.length() > 15) {
-//			if(ip.indexOf(",") > 0) {
-//				ip = ip.substring(0, ip.indexOf(","));
-//			}
-//		}
-        
-        return ip;
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
     }
-	
+    /**
+     * 判断是否为内网
+     * @param ipAddress
+     * @return
+     */
+    public static boolean isInnerIP(String ipAddress) {
+        boolean isInnerIp = false;
+        if (ipAddress.indexOf(",") != -1) {
+            ipAddress = ipAddress.split(",")[0];
+        }
+        ipAddress = ipAddress.replaceAll(",", ".").replaceAll(" ", "");
+        long ipNum = getIpNum(ipAddress);
+        /**
+         * 私有IP：
+         * A类 10.0.0.0-10.255.255.255
+         * B类  172.16.0.0-172.31.255.255
+         * C类 192.168.0.0-192.168.255.255
+         当然，还有127这个网段是环回地址
+         **/
+        long aBegin = getIpNum("10.0.0.0");
+        long aEnd = getIpNum("10.255.255.255");
+        long bBegin = getIpNum("172.16.0.0");
+        long bEnd = getIpNum("172.31.255.255");
+        long cBegin = getIpNum("192.168.0.0");
+        long cEnd = getIpNum("192.168.255.255");
+        isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd) || isInner(ipNum, cBegin, cEnd)
+                || ipAddress.equals("127.0.0.1");
+        return isInnerIp;
+    }
+
+    private static long getIpNum(String ipAddress) {
+        String[] ip = ipAddress.split("\\.");
+        long a = Integer.parseInt(ip[0]);
+        long b = Integer.parseInt(ip[1]);
+        long c = Integer.parseInt(ip[2]);
+        long d = Integer.parseInt(ip[3]);
+
+        long ipNum = a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
+        return ipNum;
+    }
+
+    private static boolean isInner(long userIp, long begin, long end) {
+        return (userIp >= begin) && (userIp <= end);
+    }
+
+
+
 }
