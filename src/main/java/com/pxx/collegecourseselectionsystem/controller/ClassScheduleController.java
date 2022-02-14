41af94542c54cad2bd30a920524aafb693eb133f
pengxingxie <ezc@live.com>
2022-02-14 17:29:27 +0800

    优化课程表下载

--- src/main/java/com/pxx/collegecourseselectionsystem/controller/ClassScheduleController.java
+++ src/main/java/com/pxx/collegecourseselectionsystem/controller/ClassScheduleController.java
@@ -103,12 +103,6 @@ public class ClassScheduleController {
         }
         // 通过工具类创建writer，默认创建xls格式
         ExcelWriter writer = ExcelUtil.getWriter();
-
-        //response为HttpServletResponse对象
-        httpServletResponse.setContentType("application/vnd.ms-excel;charset=utf-8");
-        //test.xls是弹出下载对话框的文件名，不能为中文，中文请自行编码
-        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=课程表.xls");
-        ServletOutputStream out = httpServletResponse.getOutputStream();
         //跳过当前行，既第一行，非必须，在此演示用
         writer.passCurrentRow();
         //合并单元格后的标题行，使用默认标题样式
@@ -117,6 +111,13 @@ public class ClassScheduleController {
         writer.merge(6, 6, 0, listList.get(0).size() - 1, "", true);
         // 一次性写出内容，使用默认样式，强制输出标题
         writer.write(listList, true);
+
+        //response为HttpServletResponse对象
+        httpServletResponse.setContentType("application/vnd.ms-excel;charset=utf-8");
+        //test.xls是弹出下载对话框的文件名，不能为中文，中文请自行编码
+        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=课程表.xls");
+        ServletOutputStream out = httpServletResponse.getOutputStream();
+
         writer.flush(out, true);
         // 关闭writer，释放内存
         writer.close();
